d492b5b7146c64018582dba11f4b3bd912c1c12b
AlphanetRA <alphanetra0321@gmail.com>
2022-02-07 15:37:34 +0900

    Add env, setup config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,12 +12,53 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const getHDWallet = () => {
+  const { MNEMONIC, PRIVATE_KEY } = process.env;
+
+  if (PRIVATE_KEY && PRIVATE_KEY !== "") {
+    return [PRIVATE_KEY];
+  }
+  if (MNEMONIC && MNEMONIC !== "") {
+    return {
+      mnemonic: MNEMONIC,
+    };
+  }
+
+  throw Error("Private Key Not Set! Please set up .env");
+};
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  networks: {
+    development: {
+      url: "http://localhost:8545",
+      accounts: getHDWallet(),
+    },
+    bscTestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: getHDWallet(),
+    },
+    bsc: {
+      url: "http://bsc-dataseed.binance.org",
+      accounts: getHDWallet(),
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: {
+      // binance smart chain
+      bsc: process.env.BSCSCAN_API_KEY,
+      bscTestnet: process.env.BSCSCAN_API_KEY,
+    }
+  }
 };
