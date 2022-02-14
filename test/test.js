2768460bcfcd9c6bc6f7291cae52a671f7a1d62f
AlphanetRA <alphanetra0321@gmail.com>
2022-02-14 15:45:57 +0900

    Add ProfitShareRental test script

--- test/test.js
+++ test/test.js
@@ -4,10 +4,10 @@ const utils = require("./helpers/utils")
 const time = require("./helpers/time")
 
 describe("FixedRental", function () {
-  let unicorn, paymentToken, profitToken, fixedRental;
+  let unicorn, paymentToken, fixedRental, rentFee = 100;
 
   beforeEach(async () => {
-    [ , profitTokenAdmin, beneficiary, fixedRentalAdmin, Alice, Bob] = await ethers.getSigners();
+    [ , beneficiary, fixedRentalAdmin, Alice, Bob] = await ethers.getSigners();
 
     const Unicorn = await ethers.getContractFactory("Unicorn");
     unicorn = await Unicorn.deploy();
@@ -19,18 +19,15 @@ describe("FixedRental", function () {
     await paymentToken.transfer(Alice.address, 10000);
     await paymentToken.transfer(Bob.address, 10000);
 
-    const ProfitToken = await hre.ethers.getContractFactory("ProfitToken");
-    profitToken = await ProfitToken.deploy(profitTokenAdmin.address);
-
     const FixedRental = await ethers.getContractFactory("FixedRental");
     fixedRental = await FixedRental.deploy(
       unicorn.address,
       paymentToken.address,
       beneficiary.address,
       fixedRentalAdmin.address,
-      100
+      rentFee
     );
-	})
+	});
 
   it("Should be able to lend a unicorn", async function () {
     let tokenIdAlice = 0;
@@ -41,12 +38,12 @@ describe("FixedRental", function () {
     let lendingIdBob = 2;
 
     await unicorn.connect(Alice).approve(fixedRental.address, tokenIdAlice);
-    await expect(fixedRental.connect(Alice).lend(tokenIdAlice, rentDuration, rentPrice))
+    expect(await fixedRental.connect(Alice).lend(tokenIdAlice, rentDuration, rentPrice))
           .to.emit(fixedRental, "Lend")
           .withArgs(Alice.address, tokenIdAlice, lendingIdAlice, rentDuration, rentPrice);
 
     await unicorn.connect(Bob).approve(fixedRental.address, tokenIdBob);
-    await expect(fixedRental.connect(Bob).lend(tokenIdBob, rentDuration, rentPrice))
+    expect(await fixedRental.connect(Bob).lend(tokenIdBob, rentDuration, rentPrice))
           .to.emit(fixedRental, "Lend")
           .withArgs(Bob.address, tokenIdBob, lendingIdBob, rentDuration, rentPrice);
   });
@@ -63,9 +60,11 @@ describe("FixedRental", function () {
 
     await paymentToken.connect(Bob).approve(fixedRental.address, rentPrice);
     let block = await ethers.provider.getBlock();
-    await expect(fixedRental.connect(Bob).rent(tokenId, lendingId))
+    expect(await fixedRental.connect(Bob).rent(tokenId, lendingId))
           .to.emit(fixedRental, "Rent")
           .withArgs(Bob.address, lendingId, rentingId, rentDuration, block.timestamp + 1);
+
+    expect(await paymentToken.balanceOf(beneficiary.address)).to.equal(rentFee);
   });
 
   it("Should not be able to rent a unicorn by lender", async function () {
@@ -95,10 +94,12 @@ describe("FixedRental", function () {
     await paymentToken.connect(Bob).approve(fixedRental.address, rentPrice);
     await fixedRental.connect(Bob).rent(tokenId, lendingId);
 
+    expect(await paymentToken.balanceOf(beneficiary.address)).to.equal(rentFee);
+
     await time.increase(time.duration.days(rentDuration));
 
     let block = await ethers.provider.getBlock();
-    await expect(fixedRental.connect(Bob).claimRent(tokenId, lendingId, rentingId))
+    expect(await fixedRental.connect(Bob).claimRent(tokenId, lendingId, rentingId))
           .to.emit(fixedRental, "RentClaimed")
           .withArgs(rentingId, block.timestamp + 1);
   });
@@ -116,6 +117,8 @@ describe("FixedRental", function () {
     await paymentToken.connect(Bob).approve(fixedRental.address, rentPrice);
     await fixedRental.connect(Bob).rent(tokenId, lendingId);
 
+    expect(await paymentToken.balanceOf(beneficiary.address)).to.equal(rentFee);
+
     await time.increase(time.duration.days(rentDuration));
 
     await utils.shouldThrow(fixedRental.connect(Alice).claimRent(tokenId, lendingId, rentingId));
@@ -132,7 +135,7 @@ describe("FixedRental", function () {
       await fixedRental.connect(Alice).lend(tokenId, rentDuration, rentPrice);
 
       let block = await ethers.provider.getBlock();
-      await expect(fixedRental.connect(Alice).stopLend(tokenId, lendingId))
+      expect(await fixedRental.connect(Alice).stopLend(tokenId, lendingId))
             .to.emit(fixedRental, "StopLend")
             .withArgs(lendingId, block.timestamp + 1)
     });
@@ -147,6 +150,8 @@ describe("FixedRental", function () {
 
       await paymentToken.connect(Bob).approve(fixedRental.address, rentPrice);
       await fixedRental.connect(Bob).rent(tokenId, lendingId);
+
+      expect(await paymentToken.balanceOf(beneficiary.address)).to.equal(rentFee);
 
       await utils.shouldThrow(fixedRental.connect(Alice).stopLend(tokenId, lendingId));
     });
@@ -163,15 +168,215 @@ describe("FixedRental", function () {
       await paymentToken.connect(Bob).approve(fixedRental.address, rentPrice);
       await fixedRental.connect(Bob).rent(tokenId, lendingId);
 
+      expect(await paymentToken.balanceOf(beneficiary.address)).to.equal(rentFee);
+
       await time.increase(time.duration.days(rentDuration));
 
       await fixedRental.connect(Bob).claimRent(tokenId, lendingId, rentingId);
       
       let block = await ethers.provider.getBlock();
-      await expect(fixedRental.connect(Alice).stopLend(tokenId, lendingId))
+      expect(await fixedRental.connect(Alice).stopLend(tokenId, lendingId))
             .to.emit(fixedRental, "StopLend")
             .withArgs(lendingId, block.timestamp + 1)
     });
   });
 });
 
+describe("ProfitShareRental", function () {
+  let unicorn, paymentToken, profitToken, profitShareRental, rentFee = 100;
+
+  beforeEach(async () => {
+    [ , profitTokenAdmin, beneficiary, profitShareRentalAdmin, Alice, Bob] = await ethers.getSigners();
+
+    const Unicorn = await ethers.getContractFactory("Unicorn");
+    unicorn = await Unicorn.deploy();
+    await unicorn.connect(Alice).mint();
+    await unicorn.connect(Bob).mint();
+
+    const PaymentToken = await hre.ethers.getContractFactory("PaymentToken");
+    paymentToken = await PaymentToken.deploy();
+    await paymentToken.transfer(Alice.address, 10000);
+    await paymentToken.transfer(Bob.address, 10000);
+
+    const ProfitToken = await hre.ethers.getContractFactory("ProfitToken");
+    profitToken = await ProfitToken.deploy(profitTokenAdmin.address);
+
+    const ProfitShareRental = await ethers.getContractFactory("ProfitShareRental");
+    profitShareRental = await ProfitShareRental.deploy(
+      unicorn.address,
+      paymentToken.address,
+      profitToken.address,
+      beneficiary.address,
+      profitShareRentalAdmin.address,
+      rentFee
+    );
+	});
+
+  it("Should be able to lend a unicorn", async function () {
+    let tokenIdAlice = 0;
+    let tokenIdBob = 1;
+    let profitPercentageToRenter = 10;
+    let lendingIdAlice = 1;
+    let lendingIdBob = 2;
+
+    await unicorn.connect(Alice).approve(profitShareRental.address, tokenIdAlice);
+    expect(await profitShareRental.connect(Alice).lend(tokenIdAlice, profitPercentageToRenter))
+          .to.emit(profitShareRental, "Lend")
+          .withArgs(Alice.address, tokenIdAlice, lendingIdAlice, profitPercentageToRenter);
+
+    await unicorn.connect(Bob).approve(profitShareRental.address, tokenIdBob);
+    expect(await profitShareRental.connect(Bob).lend(tokenIdBob, profitPercentageToRenter))
+          .to.emit(profitShareRental, "Lend")
+          .withArgs(Bob.address, tokenIdBob, lendingIdBob, profitPercentageToRenter);
+  });
+
+  it("Should be able to rent a unicorn", async function () {
+    let tokenId = 0;
+    let profitPercentageToRenter = 10;
+    let lendingId = 1;
+    let rentingId = 1;
+
+    await unicorn.connect(Alice).approve(profitShareRental.address, tokenId);
+    await profitShareRental.connect(Alice).lend(tokenId, profitPercentageToRenter);
+
+    await paymentToken.connect(Bob).approve(profitShareRental.address, rentFee);
+    let block = await ethers.provider.getBlock();
+    expect(await profitShareRental.connect(Bob).rent(tokenId, lendingId))
+          .to.emit(profitShareRental, "Rent")
+          .withArgs(Bob.address, lendingId, rentingId, block.timestamp + 1);
+
+    expect(await paymentToken.balanceOf(beneficiary.address)).to.equal(rentFee);
+  });
+
+  it("Should not be able to rent a unicorn by lender", async function () {
+    let tokenId = 0;
+    let profitPercentageToRenter = 10;
+    let lendingId = 1;
+    let rentingId = 1;
+
+    await unicorn.connect(Alice).approve(profitShareRental.address, tokenId);
+    await profitShareRental.connect(Alice).lend(tokenId, profitPercentageToRenter);
+
+    await paymentToken.connect(Alice).approve(profitShareRental.address, rentFee);
+    await utils.shouldThrow(profitShareRental.connect(Alice).rent(tokenId, lendingId));
+  });
+
+  it("Should be able to stop a rent by renter", async function () {
+    let tokenId = 0;
+    let profitPercentageToRenter = 10;
+    let lendingId = 1;
+    let rentingId = 1;
+
+    await unicorn.connect(Alice).approve(profitShareRental.address, tokenId);
+    await profitShareRental.connect(Alice).lend(tokenId, profitPercentageToRenter);
+
+    await paymentToken.connect(Bob).approve(profitShareRental.address, rentFee);
+    await profitShareRental.connect(Bob).rent(tokenId, lendingId);
+
+    expect(await paymentToken.balanceOf(beneficiary.address)).to.equal(rentFee);
+
+    let block = await ethers.provider.getBlock();
+    expect(await profitShareRental.connect(Bob).claimRent(tokenId, lendingId, rentingId))
+          .to.emit(profitShareRental, "RentClaimed")
+          .withArgs(rentingId, block.timestamp + 1);
+  });
+
+  it("Should be able to stop a rent by lender", async function () {
+    let tokenId = 0;
+    let profitPercentageToRenter = 10;
+    let lendingId = 1;
+    let rentingId = 1;
+
+    await unicorn.connect(Alice).approve(profitShareRental.address, tokenId);
+    await profitShareRental.connect(Alice).lend(tokenId, profitPercentageToRenter);
+
+    await paymentToken.connect(Bob).approve(profitShareRental.address, rentFee);
+    await profitShareRental.connect(Bob).rent(tokenId, lendingId);
+
+    expect(await paymentToken.balanceOf(beneficiary.address)).to.equal(rentFee);
+
+    let block = await ethers.provider.getBlock();
+    expect(await profitShareRental.connect(Alice).claimRent(tokenId, lendingId, rentingId))
+          .to.emit(profitShareRental, "RentClaimed")
+          .withArgs(rentingId, block.timestamp + 1);
+  });
+
+  context("Should be able to stop free lend", async () => {
+    it("After lending a unicorn, should be able to stop it again", async function () {
+      let tokenId = 0;
+      let profitPercentageToRenter = 10;
+      let lendingId = 1;
+      let rentingId = 1;
+
+      await unicorn.connect(Alice).approve(profitShareRental.address, tokenId);
+      await profitShareRental.connect(Alice).lend(tokenId, profitPercentageToRenter);
+
+      let block = await ethers.provider.getBlock();
+      expect(await profitShareRental.connect(Alice).stopLend(tokenId, lendingId))
+            .to.emit(profitShareRental, "StopLend")
+            .withArgs(lendingId, block.timestamp + 1);
+    });
+    it("After lending a unicorn and renting it, should not be able to stop that lending", async function () {
+      let tokenId = 0;
+      let profitPercentageToRenter = 10;
+      let lendingId = 1;
+      let rentingId = 1;
+
+      await unicorn.connect(Alice).approve(profitShareRental.address, tokenId);
+      await profitShareRental.connect(Alice).lend(tokenId, profitPercentageToRenter);
+
+      await paymentToken.connect(Bob).approve(profitShareRental.address, rentFee);
+      await profitShareRental.connect(Bob).rent(tokenId, lendingId);
+
+      expect(await paymentToken.balanceOf(beneficiary.address)).to.equal(rentFee);
+
+      await utils.shouldThrow(profitShareRental.connect(Alice).stopLend(tokenId, lendingId));
+    });
+    it("After lending a unicorn, renting it and stopping renting, should be able to stop that lending", async function () {
+      let tokenId = 0;
+      let profitPercentageToRenter = 10;
+      let lendingId = 1;
+      let rentingId = 1;
+
+      await unicorn.connect(Alice).approve(profitShareRental.address, tokenId);
+      await profitShareRental.connect(Alice).lend(tokenId, profitPercentageToRenter);
+
+      await paymentToken.connect(Bob).approve(profitShareRental.address, rentFee);
+      await profitShareRental.connect(Bob).rent(tokenId, lendingId);
+
+      expect(await paymentToken.balanceOf(beneficiary.address)).to.equal(rentFee);
+
+      await profitShareRental.connect(Bob).claimRent(tokenId, lendingId, rentingId);
+      
+      let block = await ethers.provider.getBlock();
+      expect(await profitShareRental.connect(Alice).stopLend(tokenId, lendingId))
+            .to.emit(profitShareRental, "StopLend")
+            .withArgs(lendingId, block.timestamp + 1)
+    });
+  });
+
+  it("Should be able to share the profit", async function () {
+    let tokenId = 0;
+    let profitPercentageToRenter = 10;
+    let lendingId = 1;
+    let rentingId = 1;
+    let shareAmount = 1000;
+    let amountToBob = profitPercentageToRenter * shareAmount / 100;
+    let amountToAlice = shareAmount - amountToBob;
+
+    await unicorn.connect(Alice).approve(profitShareRental.address, tokenId);
+    await profitShareRental.connect(Alice).lend(tokenId, profitPercentageToRenter);
+
+    await paymentToken.connect(Bob).approve(profitShareRental.address, rentFee);
+    await profitShareRental.connect(Bob).rent(tokenId, lendingId);
+
+    expect(await paymentToken.balanceOf(beneficiary.address)).to.equal(rentFee);
+
+    await profitToken.connect(profitTokenAdmin).mint(profitShareRental.address, shareAmount);
+    
+    await profitShareRental.connect(Bob).distributeProfit(tokenId, lendingId, rentingId, shareAmount);
+
+    expect(await profitToken.balanceOf(Alice.address)).to.equal(amountToAlice);
+    expect(await profitToken.balanceOf(Bob.address)).to.equal(amountToBob);
+  });
+});
